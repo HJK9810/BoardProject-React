b34267c6a264d63c81fb47851158c5b25f0d0005
HJ_Kim <ruby45342@gmail.com>
2022-12-26 11:29:37 +0900

    add reissue btn

--- src/screen/expire.screen.jsx
+++ src/screen/expire.screen.jsx
@@ -1,9 +1,32 @@
 import {Container} from "react-bootstrap";
+import {useCookies} from "react-cookie";
 import {useNavigate} from "react-router-dom";
 import Header from "../layout/Header";
+import BoardService from "../service/BoardService";
 
 function ExpireLogin() {
   const navigate = useNavigate();
+  const [cookie, setCookie] = useCookies("[token]");
+
+  const reissue = async (e) => {
+    e.preventDefault();
+    const token = cookie.token;
+
+    const body = {
+      accessToken: token,
+      refreshToken: cookie.refreshToken,
+    };
+    console.log(body);
+
+    // 토큰 갱신 서버통신
+    await BoardService.refreshToken(body, token).then((res) => {
+      setCookie("refreshToken", res.data.refreshToken);
+      setCookie("exp", res.data.accessTokenExpiresIn);
+      setCookie("token", res.data.accessToken);
+    });
+
+    navigate("/board");
+  };
 
   return (
     <Container className="text-center">
@@ -12,6 +35,9 @@ function ExpireLogin() {
       <h5 className="p-1">다시 로그인 하거나 시간을 연장해 주세요.</h5>
       <button className="btn btn-outline-success m-3" onClick={(e) => navigate("/login")}>
         로그인 하기
+      </button>
+      <button className="btn btn-outline-success m-3" onClick={reissue}>
+        연장하기
       </button>
     </Container>
   );

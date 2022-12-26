194218f3e21b85bf6cd1788cc007de93b8c7259e
HJ_Kim <ruby45342@gmail.com>
2022-12-26 18:00:40 +0900

    set replace when move

--- src/screen/expire.screen.jsx
+++ src/screen/expire.screen.jsx
@@ -32,7 +32,7 @@ function ExpireLogin() {
       Axios.defaults.headers.common["Authorization"] = `Bearer ${res.data.accessToken}`;
 
       if (!res.data) setBtnWork(false);
-      else navigate("/board");
+      else navigate("/board", {replace: true});
     });
   };
 
@@ -41,7 +41,7 @@ function ExpireLogin() {
       <Header headline={"Login Please"} />
       <h3 className="p-2 pt-5 mt-5">로그인 시간이 만료되었습니다.</h3>
       <h5 className="p-1">다시 로그인 하거나 시간을 연장해 주세요.</h5>
-      <button className="btn btn-outline-success m-3" onClick={(e) => navigate("/login")}>
+      <button className="btn btn-outline-success m-3" onClick={(e) => navigate("/login", {replace: true})}>
         로그인 하기
       </button>
       <button className="btn btn-outline-success m-3" onClick={reissue} disabled={btnWork ? "" : "disabled"}>

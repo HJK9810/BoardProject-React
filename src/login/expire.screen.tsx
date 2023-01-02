2af392a0d23f8826e12baf8e1d3565281ff09fb5
HJ_Kim <ruby45342@gmail.com>
2023-01-02 09:40:58 +0900

    expire css change

--- src/login/expire.screen.tsx
+++ src/login/expire.screen.tsx
@@ -46,8 +46,8 @@ function ExpireLogin() {
   return (
     <div className="text-center container">
       <Header headline={"Login Please"} />
-      <h3 className="p-2 pt-5 mt-5">로그인 시간이 만료되었습니다.</h3>
-      <h4 className="p-1">다시 로그인 하거나 시간을 연장해 주세요.</h4>
+      <h2 className="p-2 pt-5 mt-5">로그인 시간이 만료되었습니다.</h2>
+      <h3 className="p-1">다시 로그인 하거나 시간을 연장해 주세요.</h3>
       <button className="btn btn-outline-success m-3" onClick={(e) => navigate("/login", {replace: true})}>
         로그인 하기
       </button>
